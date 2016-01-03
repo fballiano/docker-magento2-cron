a69ea40fea9cb3428e3deb78e88dfdb92d36bcf4
Fabrizio Balliano <fabrizio@fabrizioballiano.com>
2016-01-03 18:20:49 +0000

    variable name typo

--- updatenodes.php
+++ updatenodes.php
@@ -40,7 +40,7 @@ ksort($apaches);
 
 $hosts_to_add_to_vcl = "";
 foreach ($apaches as $name=>$ip) {
-  $hosts_to_add_to_vcl .= "backend $name {\n\t.host = \"$ip\";\n\t.port = \"80\";\n\t.probe = {.url = \"{$probe_files}\";.timeout = 1s;.interval = 5s;.window = 1;.threshold = 1;}\n}\n";
+  $hosts_to_add_to_vcl .= "backend $name {\n\t.host = \"$ip\";\n\t.port = \"80\";\n\t.probe = {.url = \"{$probe_file}\";.timeout = 1s;.interval = 5s;.window = 1;.threshold = 1;}\n}\n";
 }
 
 $hosts_to_add_to_vcl .= "sub vcl_init {\n\tnew cluster1 = directors.round_robin();\n";
